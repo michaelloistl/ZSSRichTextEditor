7e7aa50d4347e35a8b5836522caf669806e5939c
Michael Loistl <michael@aplo.co>
2015-10-29 14:43:26 +0000

    Commenting out some NSLogs

--- ZSSRichTextEditor/Source/ZSSRichTextViewController.m
+++ ZSSRichTextEditor/Source/ZSSRichTextViewController.m
@@ -1139,11 +1139,9 @@ static void BSAttachAccessoryToWebView(UIWebView* webView,UIView* accessoryView)
 
 - (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
     
-    
-    
     NSString *urlString = [[request URL] absoluteString];
-    NSLog(@"web request");
-    NSLog(@"%@", urlString);
+//    NSLog(@"web request");
+//    NSLog(@"%@", urlString);
     if (navigationType == UIWebViewNavigationTypeLinkClicked) {
         return NO;
     } else if ([urlString rangeOfString:@"zss-callback://0/"].location != NSNotFound) {
