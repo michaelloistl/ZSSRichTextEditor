05203afe66168248b6a0c826f4787acb0273391d
Michael Loistl <michael@aplo.co>
2015-10-29 14:42:44 +0000

    Commenting unused test template out as XCTest.h causes issues with Xcode 7

--- ZSSRichTextEditorTests/ZSSRichTextEditorTests.m
+++ ZSSRichTextEditorTests/ZSSRichTextEditorTests.m
@@ -6,35 +6,35 @@
 //  Copyright (c) 2015 Zed Said Studio. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-#import <XCTest/XCTest.h>
-
-@interface ZSSRichTextEditorTests : XCTestCase
-
-@end
-
-@implementation ZSSRichTextEditorTests
-
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
-
-@end
+//#import <UIKit/UIKit.h>
+//#import <XCTest/XCTest.h>
+//
+//@interface ZSSRichTextEditorTests : XCTestCase
+//
+//@end
+//
+//@implementation ZSSRichTextEditorTests
+//
+//- (void)setUp {
+//    [super setUp];
+//    // Put setup code here. This method is called before the invocation of each test method in the class.
+//}
+//
+//- (void)tearDown {
+//    // Put teardown code here. This method is called after the invocation of each test method in the class.
+//    [super tearDown];
+//}
+//
+//- (void)testExample {
+//    // This is an example of a functional test case.
+//    XCTAssert(YES, @"Pass");
+//}
+//
+//- (void)testPerformanceExample {
+//    // This is an example of a performance test case.
+//    [self measureBlock:^{
+//        // Put the code you want to measure the time of here.
+//    }];
+//}
+//
+//@end
